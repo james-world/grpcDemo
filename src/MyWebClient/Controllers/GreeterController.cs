f4466b75fd1172524c8af76695715323cfc4c678
James World <james-world@users.noreply.github.com>
2020-08-08 17:29:22 +0100

    Add REST GreeterController that calls gRpc service

---
+++ src/MyWebClient/Controllers/GreeterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyService;
+
+namespace MyWebClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreeterController : ControllerBase
+    {
+        private readonly Greeter.GreeterClient _client;
+
+        public GreeterController(Greeter.GreeterClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("{name}")]
+        public async Task<string> GetGreeting(string name)
+        {
+            var reply = await _client.SayHelloAsync(new HelloRequest { Name = name });
+            return reply.Message;
+        }
+    }
+}
